5b1ef14b641ad6ba9a831a6a15181eb60bedb66c
IvanDrv <ivander05@mail.ru>
2025-07-12 11:24:09 +0300

    Data transfer object for request results

---
+++ demo/src/main/java/com/webchipherapp/demo/DataTransferObjects/CipherResultDTO.java
@@ -0,0 +1,12 @@
+package com.webchipherapp.demo.DataTransferObjects;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CipherResultDTO {
+    private String text;
+}
