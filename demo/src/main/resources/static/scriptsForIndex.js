94e9a3bb50cc337d15716f20dd6dc02a2ff6cf80
IvanDrv <ivander05@mail.ru>
2025-07-12 15:59:51 +0300

    scripts for processing HTTP requests

---
+++ demo/src/main/resources/static/scriptsForIndex.js
@@ -0,0 +1,66 @@
+async function encrypt() {
+    const inp1 = document.getElementById("inp1");
+    const key = document.getElementById("inp2");
+    const res = document.getElementById("res");
+
+    inputText = inp1.value;
+    inputKey = key.value;
+    
+    try {
+        const response = await fetch('http://localhost:8080/api/processVigenerCipherEncryptionRequest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                text: inputText,
+                key: inputKey
+                })
+        });
+
+        if (!response.ok) throw new Error('Ошибка сервера');
+        
+        const result = await response.text();
+        alert(result);
+        res.value = JSON.parse(result).text;
+
+    } catch (error) {
+        console.log(`Ошибка: ${error.message}`);
+    };
+};
+
+async function decrypt() {
+    const inp1 = document.getElementById("inp1");
+    const key = document.getElementById("inp2");
+    const res = document.getElementById("res");
+
+    inputText = inp1.value;
+    inputKey = key.value;
+    
+    try {
+        const response = await fetch('http://localhost:8080/api/processVigenerCipherDecryptionRequest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                text: inputText,
+                key: inputKey
+                })
+        });
+
+        if (!response.ok) throw new Error('Ошибка сервера');
+        
+        const result = await response.text();
+        alert(result);
+        res.value = JSON.parse(result).text;
+
+    } catch (error) {
+        console.log(`Ошибка: ${error.message}`);
+    };
+};
+
+function clearResult() {
+    const resField = document.getElementById("res");
+    resField.value = "";
+}
