8573542a3c8f95160a3b9e6c23498518f7df33e7
IvanDrv <ivander05@mail.ru>
2025-07-13 15:59:03 +0300

    Tests moved to Spring auto testing file

--- demo/src/main/java/com/webchipherapp/demo/Model/CipherManager.java
+++ demo/src/main/java/com/webchipherapp/demo/Model/CipherManager.java
@@ -10,16 +10,6 @@ public class CipherManager {
     final VigenerCipher vigenerCipherModel = new VigenerCipher();
     final CaesarCipher caesarCipherModel = new CaesarCipher();
 
-    public static void main(String[] args) {
-        CipherManager testObj = new CipherManager();
-        CipherRequestDTO testDTO = new CipherRequestDTO("QWerTY TXT 1234 .m, n!", "key");
-        CipherResultDTO res = testObj.handleVigenerCipherEncryptionRequest(testDTO);
-        System.out.println(res);
-
-        CipherRequestDTO decryptDTO = new CipherRequestDTO(res.getText(), "key");
-        System.out.println(testObj.handleVigenerCipherDecryptionRequest(decryptDTO));
-    }
-
     public CipherResultDTO handleVigenerCipherEncryptionRequest(CipherRequestDTO dto){
         return new CipherResultDTO(vigenerCipherModel.encryptText(dto.getText(), dto.getKey()));
     }
