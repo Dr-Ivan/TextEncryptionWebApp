9ad47fff415b3f7a14f48cc16becc2c1ea40ef34
IvanDrv <ivander05@mail.ru>
2025-07-13 16:04:45 +0300

    Preparation for one more model component

---
+++ demo/src/main/java/com/webchipherapp/demo/Model/CaesarCipher.java
@@ -0,0 +1,17 @@
+package com.webchipherapp.demo.Model;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class CaesarCipher implements CipherInterface{
+    @Override
+    public String encryptText(String text, String key) {
+        return "";
+    }
+
+    @Override
+    public String decryptText(String text, String key) {
+
+        return "";
+    }
+}
