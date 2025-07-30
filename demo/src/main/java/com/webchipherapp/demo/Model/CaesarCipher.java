0e5dc93e8a19f08cc790592c4b351dd851401854
IvanDrv <ivander05@mail.ru>
2025-07-30 19:49:20 +0300

    Letter encoding functions

--- demo/src/main/java/com/webchipherapp/demo/Model/CaesarCipher.java
+++ demo/src/main/java/com/webchipherapp/demo/Model/CaesarCipher.java
@@ -2,10 +2,13 @@ package com.webchipherapp.demo.Model;
 
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+
 @NoArgsConstructor
 public class CaesarCipher implements CipherInterface{
     @Override
     public String encryptText(String text, String key) {
+
         return "";
     }
 
@@ -13,5 +16,21 @@ public class CaesarCipher implements CipherInterface{
     public String decryptText(String text, String key) {
 
         return "";
+    }
+
+    private char encodeEnglishLetter(char letter, int key){
+        return (char) ((letter - 'a' + key) % 26 + 'a');
+    }
+
+    private char encodeRussianLetter(char letter, int key){
+        return (char) ((letter - 'а' + key) % 33 + 'а'); // используется А из кириллицы
+    }
+
+    private char decodeEnglishLetter(char letter, int key){
+        return (char) ((letter - 'a' - key) % 26 + 'a');
+    }
+
+    private char decodeRussianLetter(char letter, int key){
+        return (char) ((letter - 'а' - key) % 33 + 'а'); // используется А из кириллицы
     }
 }
