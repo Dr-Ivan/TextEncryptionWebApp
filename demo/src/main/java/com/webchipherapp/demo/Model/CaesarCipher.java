edd37c9345012c885a2bec990efbcb55a1a512a1
IvanDrv <ivander05@mail.ru>
2025-07-31 15:02:08 +0300

    Decoding letters fixed

--- demo/src/main/java/com/webchipherapp/demo/Model/CaesarCipher.java
+++ demo/src/main/java/com/webchipherapp/demo/Model/CaesarCipher.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 import java.util.HashSet;
 
 @NoArgsConstructor
-public class CaesarCipher implements CipherInterface{
+public class CaesarCipher implements CipherInterface {
     @Override
     public String encryptText(String text, String key) {
         if (key.isEmpty()) return "Key must contain at least one digit!";
@@ -13,21 +13,21 @@ public class CaesarCipher implements CipherInterface{
         int textLen = text.length();
         int keyLen = key.length();
         char c;
-        for (int i = 0; i < keyLen; ++i){
+        for (int i = 0; i < keyLen; ++i) {
             c = key.charAt(i);
             if (!Character.isDigit(c)) return "Key must contain only digits!";
         }
         int keyInt = Integer.parseInt(key);
 
         HashSet<Integer> capitalIndexes = new HashSet<>();
-        for (int i = 0; i < textLen; ++i){
+        for (int i = 0; i < textLen; ++i) {
             if (Character.isUpperCase(text.charAt(i))) capitalIndexes.add(i);
         }
 
-        text = text.toLowerCase();
+        text = text.toLowerCase().replace('ё', 'е');
         StringBuilder encrypted = new StringBuilder();
         char code;
-        for (int i = 0; i < textLen; ++i){
+        for (int i = 0; i < textLen; ++i) {
             c = text.charAt(i);
             if (!Character.isLetter(c)) {
                 code = c;
@@ -44,23 +44,63 @@ public class CaesarCipher implements CipherInterface{
 
     @Override
     public String decryptText(String text, String key) {
+        if (key.isEmpty()) return "Key must contain at least one digit!";
 
-        return "";
+        int textLen = text.length();
+        int keyLen = key.length();
+        char c;
+        for (int i = 0; i < keyLen; ++i) {
+            c = key.charAt(i);
+            if (!Character.isDigit(c)) return "Key must contain only digits!";
+        }
+        int keyInt = Integer.parseInt(key);
+
+        HashSet<Integer> capitalIndexes = new HashSet<>();
+        for (int i = 0; i < textLen; ++i) {
+            if (Character.isUpperCase(text.charAt(i))) capitalIndexes.add(i);
+        }
+
+        text = text.toLowerCase().replace('ё', 'е');
+        StringBuilder decrypted = new StringBuilder();
+        char code;
+        for (int i = 0; i < textLen; ++i) {
+            c = text.charAt(i);
+            if (!Character.isLetter(c)) {
+                code = c;
+            } else {
+                if (c >= 'a' && c <= 'z') code = decodeEnglishLetter(c, keyInt);
+                else if (c >= 'а' && c <= 'я') code = decodeRussianLetter(c, keyInt);
+                else code = c;
+                if (capitalIndexes.contains(i)) code = Character.toUpperCase(code);
+            }
+            decrypted.append(code);
+        }
+        return decrypted.toString();
     }
 
-    private char encodeEnglishLetter(char letter, int key){
+    private char encodeEnglishLetter(char letter, int key) {
         return (char) ((letter - 'a' + key) % 26 + 'a');
     }
 
-    private char encodeRussianLetter(char letter, int key){
-        return (char) ((letter - 'а' + key) % 33 + 'а'); // используется А из кириллицы
+    private char encodeRussianLetter(char letter, int key) {
+        return (char) ((letter - 'а' + key) % 32 + 'а'); // используется А из кириллицы
     }
 
-    private char decodeEnglishLetter(char letter, int key){
-        return (char) ((letter - 'a' - key) % 26 + 'a');
+    private char decodeEnglishLetter(char letter, int key) {
+        if (letter - 'a' - key < 0) {
+            int shift = Math.abs(letter - 'a' - key) % 26;
+            if (shift == 0) return 'a';
+            return (char) (1 + 'z' - shift);
+        } else
+            return (char) ((letter - 'a' - key) % 26 + 'a');
     }
 
-    private char decodeRussianLetter(char letter, int key){
-        return (char) ((letter - 'а' - key) % 33 + 'а'); // используется А из кириллицы
+    private char decodeRussianLetter(char letter, int key) {
+        if (letter - 'а' - key < 0) { // используется А из кириллицы
+            int shift = Math.abs(letter - 'а' - key) % 32;
+            if (shift == 0) return 'а';
+            return (char) (1 + 'я' - shift);
+        } else
+            return (char) ((letter - 'а' - key) % 32 + 'а');
     }
 }
