b120af9ef39570c454c80f4475b8e72e6e50845f
IvanDrv <ivander05@mail.ru>
2025-08-02 16:31:08 +0300

    Scripts for new page

---
+++ demo/src/main/resources/static/scriptsForCaesarCipher.js
@@ -0,0 +1,64 @@
+async function encrypt() {
+    const inp1 = document.getElementById("inp1");
+    const key = document.getElementById("inp2");
+    const res = document.getElementById("res");
+
+    inputText = inp1.value;
+    inputKey = key.value;
+    
+    try {
+        const response = await fetch('https://derevyanko.braverto.com/api/processCaesarCipherEncryptionRequest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                text: inputText,
+                key: inputKey
+                })
+        });
+
+        if (!response.ok) throw new Error('Ошибка сервера');
+        
+        const result = await response.text();
+        res.value = JSON.parse(result).text;
+
+    } catch (error) {
+        console.log(`Ошибка: ${error.message}`);
+    };
+};
+
+async function decrypt() {
+    const inp1 = document.getElementById("inp1");
+    const key = document.getElementById("inp2");
+    const res = document.getElementById("res");
+
+    inputText = inp1.value;
+    inputKey = key.value;
+    
+    try {
+        const response = await fetch('https://derevyanko.braverto.com/api/processCaesarCipherDecryptionRequest', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                text: inputText,
+                key: inputKey
+                })
+        });
+
+        if (!response.ok) throw new Error('Ошибка сервера');
+        
+        const result = await response.text();
+        res.value = JSON.parse(result).text;
+
+    } catch (error) {
+        console.log(`Ошибка: ${error.message}`);
+    };
+};
+
+function clearResult() {
+    const resField = document.getElementById("res");
+    resField.value = "";
+}
